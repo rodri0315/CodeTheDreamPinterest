b7ba60a037b54347827fa83ba3be8c58d4ff1ade
Jorge <rodri0315@gmail.com>
2015-07-27 00:16:16 -0400

    Created home and bout pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def andrea
+  end
+  
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about" #creates about_path
+  get "andrea" => "pages#andrea"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
